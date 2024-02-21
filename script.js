b3a0affc2a47e00cc471c6961cf679f2d5d9f73d
Beverly <beverly.diaz@hotmail.com>
2024-02-21 13:14:34 +0100

    Button click event

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const config = require('./config.json');
 
 
 const key = apiKey;
+
+// button click event 
+document.querySelector('button').addEventListener('click', function () {
+    console.log('ok')
+});
