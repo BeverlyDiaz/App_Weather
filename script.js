2b563d114e6cd3a3884ebcc2f8f4a224da0095fc
Beverly <beverly.diaz@hotmail.com>
2024-02-21 13:31:10 +0100

    get the user's input

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const key = apiKey;
 // button click event 
 document.querySelector('button').addEventListener('click', function () {
     // console.log('ok')
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=${key}`)
+    let city = document.querySelector('#city-name').value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
         .then(response => response.json())
         .then(data => {
             console.log(data.weather[0].main)
