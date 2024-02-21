6dc9b4e5e5e5a2a4cf15c15285a267ae3698c128
Beverly <beverly.diaz@hotmail.com>
2024-02-21 13:37:05 +0100

    Assign the weather data to a variable let

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ document.querySelector('button').addEventListener('click', function () {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
         .then(response => response.json())
         .then(data => {
+            let weather = data.weather[0].main;
             document.querySelector('.temp').textContent = weather;
            document.querySelector('.city').textContent = data.name;
            document.querySelector('img').src = `images/${weather.toLowerCase()}.png`;
