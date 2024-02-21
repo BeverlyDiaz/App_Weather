b34eab0c8c6c2bca9ffb520d634abee62295ed46
Beverly <beverly.diaz@hotmail.com>
2024-02-21 13:28:28 +0100

    replace require with import

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const config = require('./config.json');
-
+import { apiKey } from './config.mjs';
 
 const key = apiKey;
 
 // button click event 
 document.querySelector('button').addEventListener('click', function () {
-    console.log('ok')
+    // console.log('ok')
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=${key}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.weather[0].main)
+        });
+
 });
