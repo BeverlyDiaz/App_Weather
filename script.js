1b54b046e5a19709cfe059978f69f04a13a3cfe7
Beverly <beverly.diaz@hotmail.com>
2024-02-21 13:32:47 +0100

    display the data

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ document.querySelector('button').addEventListener('click', function () {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data.weather[0].main)
+            document.querySelector('.temp').textContent = weather;
+           document.querySelector('.city').textContent = data.name;
         });
 
 });
